b41f5daa01e32b473174019b59d68e953a6ae8a3
Lauren Emick <laurenemick6@gmail.com>
2020-09-24 15:26:00 -0700

    added nav icons

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-const Navigation = () => {
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedinIn} from '@fortawesome/free-brands-svg-icons';
+import { faAt } from '@fortawesome/free-solid-svg-icons';
 
+const Navigation = () => {
+// FIXME: add backend or mailtrap to send emails 
+// https://blog.mailtrap.io/react-send-email/
 	return (
 		<div className="navigation">
-			<NavLink to="/">Home</NavLink>
-            <NavLink to="/projects">Projects</NavLink>
-            <NavLink to="/experience">Experience</NavLink>
-            <NavLink to="/contact">Contact</NavLink>
+			<div className="icons">
+				<a href="https://www.linkedin.com/in/laurenemick/">
+					<FontAwesomeIcon icon={faLinkedinIn} size="2x" />
+				</a>
+				<a href="https://github.com/laurenemick">
+					<FontAwesomeIcon icon={faGithub} size="2x" /> 
+				</a>
+				<NavLink to="/contact">
+					<FontAwesomeIcon icon={faAt} size="2x" />
+				</NavLink> 
+			</div>
+
+			<div className="links">
+				<NavLink to="/">Home</NavLink>
+				<NavLink to="/projects">Projects</NavLink>
+				<NavLink to="/experience">Experience</NavLink>
+				<NavLink to="/contact">Contact</NavLink>
+			</div>
 		</div>
 	);
 };
