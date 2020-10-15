28ecef13490a3cdc38e35a34f0b3fe9906fdaa79
Lauren Emick <laurenemick6@gmail.com>
2020-10-15 15:52:30 -0700

    added download button to Experience component

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,10 +1,19 @@
 import React from "react";
+import Pdf from "react-to-pdf";
+
+const ref = React.createRef();
 
 const Experience = () => {
 
   return (
     <div id="experience" className="tab">
-        <h1>Experience Section</h1>
+      <Pdf className="resumeBtn" targetRef={ref} filename="resume.pdf">
+        {({ toPdf }) => <button onClick={toPdf}>Download</button>}
+      </Pdf>
+      <div className="resume" ref={ref} style={{margin:"2%"}}>
+        <h1 style={{marginTop:"4%"}}>Lauren Emick</h1>
+        <p>My Experience here...</p>
+      </div>
     </div>
   );
 };
